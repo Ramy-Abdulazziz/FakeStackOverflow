54be1b0abc9ff2f88b5fd7acb3aaad65c4b86a66
Ramy <ramy@Kurama>
2023-05-28 17:55:24 -0400

    question detail design

--- client/src/components/detailedQuestionPage.js
+++ client/src/components/detailedQuestionPage.js
@@ -1,6 +1,8 @@
 import { useContext, useEffect, useState } from "react";
 import QuestionContext from "./questionContext";
-
+import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
+import KeyboardArrowUpIcon from "@mui/icons-material/KeyboardArrowUp";
+import PersonIcon from "@mui/icons-material/Person";
 import {
   Box,
   Container,
@@ -9,9 +11,12 @@ import {
   Typography,
   Paper,
   Button,
+  Card,
+  CardContent,
 } from "@mui/material/";
 import { useParams } from "react-router-dom";
 import axios from "axios";
+import FormatDateText from "../dateTextUtils";
 
 function QuestionComments({ question }) {
   return <div></div>;
@@ -20,45 +25,118 @@ function QuestionComments({ question }) {
 function QuestionHeader({ question }) {
   return (
     <Paper elevation={3} sx={{ borderRadius: 2, minHeight: 300 }}>
-      <Container>
-        <Grid
-          container
-          spacing={5}
-          direction="column"
-          justifyContent={"space-evenly"}
-        >
-          <Grid item>
-            <Grid container>
+      <Container sx={{ maxWidth: 1800 }}>
+        <Grid container justifyContent={"left"} spacing={2} direction={"row"}>
+          <Grid item sx={{ maxwidth: 10 }}>
+            <Grid
+              container
+              spacing={5}
+              direction="column"
+              justifyContent={"flex-start"}
+              flexDirection={"column"}
+            >
               <Grid item>
-                <Typography variant="h4">{question.title}</Typography>
+                <Button>
+                  <KeyboardArrowUpIcon fontSize="large" />
+                </Button>
+              </Grid>
+              <Grid item sx={{ ml: 2 }}>
+                <Typography variant="h4">{question.upvotes}</Typography>
+              </Grid>
+              <Grid item>
+                <Button>
+                  <KeyboardArrowDownIcon fontSize="large" />
+                </Button>
               </Grid>
             </Grid>
           </Grid>
-          <Grid
-            item
-            sx={{
-              wordWrap: "break-word",
-            }}
-          >
-            <Typography variant="h5">{question.text}</Typography>
-          </Grid>
-          <Grid item>
+
+          <Grid item sx={{ maxWidth: 900 }}>
             <Grid
               container
-              spacing={2}
-              direction="row"
-              justifyContent="flex-start"
-              alignItems="flex-start"
-              sx={{ mb: 5 }}
+              spacing={5}
+              direction="column"
+              justifyContent={"flex-start"}
             >
-              {question.tags.map((tag, index) => (
-                <Grid item key={index}>
-                  <Button type="button" variant="contained">
+              <Grid
+                item
+                sx={{
+                  overflowWrap: "break-word",
+                  wordWrap: "break-word",
+                }}
+              >
+                <Container>
+                  <Typography variant="h4">{question.title}</Typography>
+                </Container>
+              </Grid>
+              <Grid
+                item
+                sx={{
+                  overflowWrap: "break-word",
+                  wordWrap: "break-word",
+                }}
+              >
+                <Container>
+                  <Typography variant="h5">{question.text}</Typography>
+                </Container>
+              </Grid>
+              <Grid item>
+                <Container>
+                  <Grid
+                    container
+                    spacing={2}
+                    direction="row"
+                    justifyContent="flex-start"
+                    alignItems="flex-start"
+                    sx={{ mb: 0 }}
+                  >
                     {" "}
-                    {tag.name}
-                  </Button>
+                    {question.tags.map((tag, index) => (
+                      <Grid item key={index}>
+                        <Button type="button" variant="contained">
+                          {" "}
+                          {tag.name}
+                        </Button>
+                      </Grid>
+                    ))}
+                  </Grid>
+                </Container>
+              </Grid>
+
+              <Grid item>
+                <Grid
+                  container
+                  justifyContent={"flex-end"}
+                  direction="row"
+                  sx={{ mb: 2 }}
+                >
+                  <Grid item>
+                    <Card>
+                      <CardContent>
+                        <Grid container spacing={2} direction="column">
+                          <Grid item>
+                            <Typography variant="h6">
+                              {FormatDateText.formatDateText(question.ask_date)}
+                            </Typography>
+                          </Grid>
+                          <Grid item>
+                            <Grid container spacing={2} direciton="row">
+                              <Grid item>
+                                <PersonIcon fontSize="large" />
+                              </Grid>
+                              <Grid item sx={{ mt: 0.7 }}>
+                                <Typography variant="h6">
+                                  {question.asked_by.user_name}
+                                </Typography>
+                              </Grid>
+                            </Grid>
+                          </Grid>
+                        </Grid>
+                      </CardContent>
+                    </Card>
+                  </Grid>
                 </Grid>
-              ))}
+              </Grid>
             </Grid>
           </Grid>
         </Grid>
@@ -68,7 +146,7 @@ function QuestionHeader({ question }) {
 }
 function QuestionDetail({ question }) {
   return (
-    <Container>
+    <Container sx={{ maxWidth: 1800 }}>
       <QuestionHeader question={question} />
     </Container>
   );
@@ -102,7 +180,7 @@ export default function DetailedQuestionPage() {
   }, [id]);
 
   return (
-    <Container sx={{ mt: 20 }}>
+    <Container sx={{ mt: 20, maxWidth: 1800 }}>
       {" "}
       {loading ? (
         <Skeleton variant="box">{/* <QuestionDetail /> */}</Skeleton>
